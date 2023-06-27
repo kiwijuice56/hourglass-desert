a692e7f953c619fddd20ba3b608c7936286388ee
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2023-06-27 18:51:58 -0500

    Added music

--- main/prop/player/player.gd
+++ main/prop/player/player.gd
@@ -4,4 +4,4 @@ extends Node2D
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @onready var raycast: RayCast2D = $RayCast2D
 @onready var bump_timer: Timer = $BumpTimer
-@onready var step_player: StepAudioStreamPlayer = $StepAudioStreamPlayer
+@onready var step_player: StepStreamPlayer = $StepStreamPlayer
--- main/prop/player/step_stream_player.gd
+++ main/prop/player/step_stream_player.gd
@@ -1,4 +1,4 @@
-class_name StepAudioStreamPlayer
+class_name StepStreamPlayer
 extends AudioStreamPlayer
 
 @export var sounds: Array[Resource]
