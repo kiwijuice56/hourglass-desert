664230769d0a3bb13419e1838193ef46f15f903e
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2023-07-12 12:43:04 -0500

    Edited factory sprites

--- main/main.gd
+++ main/main.gd
@@ -14,7 +14,7 @@ signal enabled
 func _ready() -> void:
 	randomize()
 	GlobalMemory.reset_memory()
-	switch_world("Factory", "2")
+	switch_world("Factory", "1")
 
 # world_name must match the name of a folder within WORLD_PATH
 func switch_world(world_name: String, anchor_name: String, transition: int = -1) -> void:
