cdba51417cce91a12213d4a946867d4be8a123e9
kiwijuice56 <alfaroeric127@gmail.com>
2023-07-12 16:29:34 -0500

    Added windmill world

--- main/ui/ui.gd
+++ main/ui/ui.gd
@@ -3,3 +3,11 @@ extends CanvasLayer
 # Root UI node, stores references to commonly accessed menus  
 
 @onready var effect_menu: EffectMenu = $EffectMenu
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen", false):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
